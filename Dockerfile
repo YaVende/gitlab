fe4380212efc0f5c51349681c86683cb2331b275
Nicolas Oga <2112.oga@gmail.com>
2017-06-16 19:20:56 -0300

    Stuff for HTTPS

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM gitlab/gitlab-ce
 COPY gitlab.rb /etc/gitlab/gitlab.rb
+COPY certs/selfsigned.crt /var/certs/
+COPY certs/selfsigned.key /var/certs/
--- gitlab.rb
+++ gitlab.rb
@@ -1,6 +1,19 @@
 # Entire settings list
 # https://gitlab.com/gitlab-org/omnibus-gitlab/raw/master/files/gitlab-config-template/gitlab.rb.template
-require 'ostruct'
+external_url ENV.fetch('VIRTUAL_HOST') if ENV['VIRTUAL_HOST']
+registry_external_url ENV.fetch('REGISTRY_VIRTUAL_HOST') if ENV['REGISTRY_VIRTUAL_HOST']
 
-external_url ENV['VIRTUAL_HOST'] if ENV['VIRTUAL_HOST']
-registry_external_url ENV['REGISTRY_VIRTUAL_HOST'] if ENV['REGISTRY_VIRTUAL_HOST']
+# For usage with letsencrypt
+if ENV['ACME_CHALLENGE_PATH']
+  nginx['custom_gitlab_server_config'] = <<-NGINX_CONF
+    location ^~ /.well-known {
+      root #{ENV.fetch('ACME_CHALLENGE_PATH')};
+    }
+  NGINX_CONF
+end
+
+if ENV['SECURE_MODE'] =~ /true|1|yes/
+  nginx['redirect_http_to_https'] = true
+  nginx['ssl_certificate']        = ENV['SSL_CERTIFICATE'] || '/var/certs/selfsigned.crt'
+  nginx['ssl_certificate_key']    = ENV['SSL_CERTIFICATE_KEY'] || '/var/certs/selfsigned.key'
+end
