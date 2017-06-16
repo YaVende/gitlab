d19671d1d607d62d6df4241cdcb36748ce3d5bdd
Nicolas Oga <2112.oga@gmail.com>
2017-06-16 18:15:27 -0300

    Changes

--- gitlab.rb
+++ gitlab.rb
@@ -2,11 +2,5 @@
 # https://gitlab.com/gitlab-org/omnibus-gitlab/raw/master/files/gitlab-config-template/gitlab.rb.template
 require 'ostruct'
 
-vars =
-  OpenStruct.new(
-    virtual_host:          ENV['VIRTUAL_HOST'],
-    registry_virtual_host: ENV['REGISTRY_VIRTUAL_HOST']
-  )
-
-external_url vars.virtual_host if vars.virtual_host
-registry_external_url vars.registry_external_url if vars.registry_external_url
+external_url ENV['VIRTUAL_HOST'] if ENV['VIRTUAL_HOST']
+registry_external_url ENV['REGISTRY_VIRTUAL_HOST'] if ENV['REGISTRY_VIRTUAL_HOST']
