830b79d520a59ec6d30fe985a2f98ed1cf8e7f6d
Nicolas Oga <2112.oga@gmail.com>
2017-06-29 21:34:37 -0300

    Make virtual hosts configurable

--- gitlab.rb
+++ gitlab.rb
@@ -3,6 +3,9 @@
 external_url ENV.fetch('VIRTUAL_HOST') if ENV['VIRTUAL_HOST']
 registry_external_url ENV.fetch('REGISTRY_VIRTUAL_HOST') if ENV['REGISTRY_VIRTUAL_HOST']
 
+nginx['custom_gitlab_server_config'] ||= ''
+registry_nginx['custom_gitlab_server_config'] ||= ''
+
 # For usage with letsencrypt
 if ENV['ACME_CHALLENGE_PATH']
   nginx_conf = <<-CONF
@@ -13,6 +16,29 @@ if ENV['ACME_CHALLENGE_PATH']
 
   nginx['custom_gitlab_server_config'] = nginx_conf
   registry_nginx['custom_gitlab_server_config'] = nginx_conf
+end
+
+# Add config for proxied domains using
+# VIRTUAL_HOSTS_CONFIG="domain:internal_address"
+if ENV['VIRTUAL_HOSTS_CONFIG']
+  ENV.fetch("PROXY_CONFIG").split(/\s*,\s*/).each do |domain_and_port|
+    domain, uri = domain_and_port.split(':', 2)
+
+    nginx['custom_gitlab_server_config'] += <<-CONF
+      server {
+        server_name #{domain};
+
+        location / {
+          proxy_redirect off;
+          proxy_set_header Host $http_host;
+          proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_pass #{uri};
+        }
+      }
+    CONF
+  end
 end
 
 if ENV['SSL_CERTIFICATE'] && ENV['SSL_CERTIFICATE_KEY']
